e00d1dbc263551755ff560361971eaafb8a564f1
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-14 18:03:51 +0000

    Completed task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""type annonated module"""
+
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """creates multiple copies"""
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
