22db2dd5eecebd9990bded37aacf4f0c3f99d434
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-14 17:41:52 +0000

    Typo error fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
 
 """type annonated module"""
 
@@ -6,4 +6,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns appropriate types"""
     return [(i, len(i)) for i in lst]
