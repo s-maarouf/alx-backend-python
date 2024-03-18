20cdc45194ed550081d66183b0cafb2217fefe7b
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-18 22:16:31 +0000

    Completed task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""async coroutine module"""
+
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """a routine that returns the list of all the delays"""
+    waiting = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return waiting
