a8d22d1dbfeb7adeed0ee3404e893217a32a29b1
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-18 23:00:37 +0000

    Fixed requirement error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """a function that measures the total execution of a function"""
     s_time = time.time()
     asyncio.run(wait_n(n, max_delay))
